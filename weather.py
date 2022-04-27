53be5458810ffb0c3b1dae35ec2d5a43144777de
Mike Tillman <59715611+miketillman95@users.noreply.github.com>
2022-04-27 12:40:00 +0000

    CLI info stated

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+import argparse
 from configparser import ConfigParser
 
 
@@ -9,8 +10,6 @@ def _get_api_key():
   return config['openweather']['api_key']
 
 
-import argparse
-from configparser import ConfigParser
 
 def read_user_cli_args():
   
@@ -19,7 +18,17 @@ def read_user_cli_args():
   parser = argparse.ArgumentParser(
     description = 'gets weather and temperature       for a city'
       )
+  parser.add_argument(
+    'city', nargs= '+', type= str, help='enter the city name'
+  )
+  parser.add_argument(
+    '-i',
+    '--imperial',
+    action='store_true',
+    help='display the temperature in imperial units'
+  )
+  parser
   return parser.parse_args()
 
-if __name__== '_main_':
+if __name__ == "__main__":
   read_user_cli_args()
